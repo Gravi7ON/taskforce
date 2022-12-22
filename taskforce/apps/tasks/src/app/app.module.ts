4894d42a7ee93e55212467add977c739d0ad3ca3
Dmitry Radionov <radionovdmr@gmail.com>
2022-12-22 17:42:24 +0500

    Сгенерирует модули для сущностей бд

--- taskforce/apps/tasks/src/app/app.module.ts
+++ taskforce/apps/tasks/src/app/app.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TaskModule } from './task/task.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { CommentModule } from './comment/comment.module';
+import { PerformerModule } from './performer/performer.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports: [TaskModule],
+  imports: [
+    TaskModule,
+    PrismaModule,
+    CategoryModule,
+    CommentModule,
+    PerformerModule,
+  ],
   controllers: [],
   providers: [],
 })
