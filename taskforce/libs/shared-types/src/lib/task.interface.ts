8eb7c13375215419ff11fc5df9d7648ab13dc950
Dmitry Radionov <radionovdmr@gmail.com>
2022-12-22 16:17:20 +0500

    Добавит интерфейсы для табличных сущностей

--- taskforce/libs/shared-types/src/lib/task.interface.ts
+++ taskforce/libs/shared-types/src/lib/task.interface.ts
@@ -1,14 +1,22 @@
+import { Category } from './category.interface';
 import { TaskStatus } from './task-status.enum';
+import { Comment } from './comment.interface';
+import { Performer } from './performer.interface';
 
 export interface Task {
-  _id: string;
-  header: string;
+  id?: string;
+  title: string;
   description: string;
-  category: string;
+  category: Category[];
   cost?: number;
   deadline?: Date;
   image?: string;
   address?: string;
   tags?: string;
   status: TaskStatus;
+  createdAt: Date;
+  updatedAt: Date;
+  userId: string;
+  comments: Comment[];
+  performers: Performer[];
 }
