1a642430731adc3ad3e9f7907597bd5d0223d874
Dmitry Radionov <radionovdmr@gmail.com>
2023-01-11 12:51:46 +0500

    Добавляет конфигурации подключения mongodb, rabbitmq в сервисе notify

--- taskforce/apps/notify/src/app/app.module.ts
+++ taskforce/apps/notify/src/app/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
-
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { NOTIFY_SERVICE_ENV_PATH } from './app.constant';
+import { getMongoDbConfig, mongoDbOptions } from '../config/mongodb.config';
+import { validateEnvironments } from './env.validation';
+import { rabbitMqOptions } from '../config/rabbitmq.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      cache: true,
+      isGlobal: true,
+      envFilePath: NOTIFY_SERVICE_ENV_PATH,
+      load: [mongoDbOptions, rabbitMqOptions],
+      validate: validateEnvironments,
+    }),
+    MongooseModule.forRootAsync(getMongoDbConfig()),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- taskforce/apps/notify/src/main.ts
+++ taskforce/apps/notify/src/main.ts
@@ -5,17 +5,26 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 
 import { AppModule } from './app/app.module';
+import { getRabbitMqConfig } from './config/rabbitmq.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get<ConfigService>(ConfigService);
+  app.connectMicroservice(getRabbitMqConfig(configService));
+
+  await app.startAllMicroservices();
+  Logger.log(`🚀 Notify service is running on`);
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
   await app.listen(port);
   Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+    `🚀 REST is running on: http://localhost:${port}/${globalPrefix}`
   );
 }
 
